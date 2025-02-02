4fa488019cffa131fd9d518444c1c75e9325a9be
kratoxzmx <zmxdehao@gmail.com>
2025-02-02 15:14:06 +0800

    1.4

--- wsl/zsh/root.sh
+++ wsl/zsh/root.sh
@@ -1,8 +1,15 @@
 #root
-cp -r /home/kratoschu/.oh-my-zsh ~/.oh-my-zsh 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+
+clear;
+env|grep zmx
+export USER=root;
+export ZDOTDIR=root;
+export USER_ZDOTDIR=root;
+env|grep root
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 rm -rf ~/.oh-my-zsh/custom;
-mv /home/kratozhu/.oh-my-zsh/custom .oh-my-zsh;
-mv /home/kratozhu/.zshrc .
+cp -r /home/kratozhu/.oh-my-zsh/custom .oh-my-zsh;
+cp /home/kratozhu/.zshrc .
+chown root:root ~/.oh-my-zsh/custom
 vim ~/.zshrc
-cp -r /home/kratozhu/.vimrc /home/kratozhu/.vim -t .
--- wsl/zsh/zsh.sh
+++ wsl/zsh/zsh.sh
@@ -3,11 +3,12 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cd ~/.oh-my-zsh/custom/plugins;
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git;
-git clone https://github.com/zsh-users/zsh-autosuggestions.git;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
 cd ~/Downloads;
 git clone https://github.com/jeremyFreeAgent/oh-my-zsh-powerline-theme.git;
 cd oh-my-zsh-powerline-theme;
-./install_in_omz.sh;
-# cp powerline.zsh-theme ~/.oh-my-zsh/custom/themes;
+# ./install_in_omz.sh;
+cp powerline.zsh-theme ~/.oh-my-zsh/custom/themes;
+cp .zshrc ~
