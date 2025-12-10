2b9fc9d29a83125056ff4a64f639524eccdd321b
kratoxzmx <zmxdehao@gmail.com>
2025-12-10 14:16:42 +0800

    add comment

--- wsl/conda.sh
+++ wsl/conda.sh
@@ -1,18 +1,21 @@
 # !/bin/bash
 
+# ubuntu python update
 pip3 install pip-review pipdeptree
 pip-review --local --interactive
 
+#install conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
 ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
 conda update --all
 
+# base update
 pip3 install pip-review
 pip-review --local --interactive
 
+# quant update
 conda create -n quant python=3.13
 conda activate quant
-
 pip3 install pip-review
 pip-review --local --interactive
