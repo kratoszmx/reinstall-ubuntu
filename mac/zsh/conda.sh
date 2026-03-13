748a10e667c37b1f76d946d9a6644b22caef8138
ddy88号机器 <ddy88haojiqi@88.local>
2026-03-13 14:46:51 +0800

    update

--- mac/zsh/conda.sh
+++ mac/zsh/conda.sh
@@ -1,1 +1,13 @@
-../../wsl/conda.sh
+# !/bin/bash
+
+brew install --cask miniforge
+conda init zsh
+conda update --all
+conda update -n base -c defaults conda
+
+# quant update
+conda create -n quant python=3.14.3
+conda activate quant
+pip3 install pip-review
+pip-review --local --interactive
+conda update --all
