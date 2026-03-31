b401dc0fbd7d6f74c8476bc1f6b7cae93d870484
ddy88号机器 <ddy88haojiqi@88.local>
2026-03-31 17:28:42 +0800

    update

---
+++ mac/brew.sh
@@ -0,0 +1,3 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask chatgpt miniforge clash-verge-rev steam google-chrome visual-studio-code longbridge-pro
