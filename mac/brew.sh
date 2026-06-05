e2b4bb6232ffd227893cdda1a03987d1fe9868ae
zmx的macm4 <zmx@zmxMac-m4.local>
2026-06-05 11:33:49 +0800

    2

--- mac/brew.sh
+++ mac/brew.sh
@@ -1,3 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install --cask chatgpt miniforge clash-verge-rev steam google-chrome visual-studio-code longbridge-pro
+brew install --cask chatgpt miniforge steam google-chrome visual-studio-code longbridge-pro
+softwareupdate --install-rosetta --agree-to-license
--- mac/conda.sh
+++ mac/conda.sh
@@ -8,6 +8,4 @@ conda update -n base -c defaults conda
 # quant update
 conda create -n quant python=3.14.3
 conda activate quant
-pip3 install pip-review
-pip-review --local --interactive
 conda update --all
